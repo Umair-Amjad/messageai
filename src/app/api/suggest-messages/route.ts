dfabbc06bebebb7ee6aa5b6f928c7bbbf485872b
umair <uamjad508@gmail.com>
2024-07-28 04:15:22 +0500

    changes in verify-code route  main branchs

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -25,39 +25,39 @@
 
 // }
 
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse,  } from "ai";
-import { NextResponse } from "next/server";
+// import OpenAI from "openai";
+// import { OpenAIStream, StreamingTextResponse,  } from "ai";
+// import { NextResponse } from "next/server";
 
-// Allow streaming responses up to 30 seconds
-const openai=new OpenAI({
-    apiKey:process.env.OPENAI_API_KEY
-})
-export const runtime ='edge';
+// // Allow streaming responses up to 30 seconds
+// const openai=new OpenAI({
+//     apiKey:process.env.OPENAI_API_KEY
+// })
+// export const runtime ='edge';
 
-export async function POST() {
-  try {
-    const prompt="what is Your Name || Which one is faovurite Languge || what is computer"
-    // const { messages } = await req.json();
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        stream: true,
-        prompt:prompt,
-      });
-    const stream=OpenAIStream(response)
+// export async function POST() {
+//   try {
+//     const prompt="what is Your Name || Which one is faovurite Languge || what is computer"
+//     // const { messages } = await req.json();
+//     const response = await openai.chat.completions.create({
+//         model: 'gpt-3.5-turbo',
+//         stream: true,
+//         prompt:prompt,
+//       });
+//     const stream=OpenAIStream(response)
 
-    return new StreamingTextResponse(stream);
-  } catch (err) {
-    if (err instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = err;
-      return NextResponse.json({
-        name,
-        status,
-        headers,
-        message,
-      });
-    }
-    console.error("An unexpected error accured", err);
-    throw err;
-  }
-}
+//     return new StreamingTextResponse(stream);
+//   } catch (err) {
+//     if (err instanceof OpenAI.APIError) {
+//       const { name, status, headers, message } = err;
+//       return NextResponse.json({
+//         name,
+//         status,
+//         headers,
+//         message,
+//       });
+//     }
+//     console.error("An unexpected error accured", err);
+//     throw err;
+//   }
+// }
